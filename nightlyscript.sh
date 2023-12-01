0133d72402b8230f50b2d9c3fdadfcc2bfad1413
Oskar Jansson <bullsaw1@gmail.com>
2023-12-01 18:10:06 +0100

    Update nightlyscript.sh

--- nightlyscript.sh
+++ nightlyscript.sh
@@ -8,4 +8,4 @@ REPOPATH=
 $PYPATH $AOCURLPATH $YEARVAR $DAYVAR;
  
 mkdir $REPOPATH/day$DAYVAR && cp $REPOPATH/template/* $REPOPATH/day$DAYVAR;
-echo -e ".SILENT:\nall:\n\t$PYPATH /home/ezjanos/Scripts/aocurl.py $(YEARVAR) $(DAYVAR) -io | $PYPATH main.py" > $REPOPATH/day$DAYVAR/Makefile
+echo -e ".SILENT:\nall:\n\t$PYPATH $AOCURLPATH $YEARVAR $DAYVAR -io | $PYPATH main.py" > $REPOPATH/day$DAYVAR/Makefile
