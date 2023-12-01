9bf2c78eed111c3850ea6b557618c0374c8b43b4
IAmBullsaw <bullsaw1@gmail.com>
2023-12-02 00:00:46 +0100

    [day01] almost, pls fix eighthree

--- day01/main.py
+++ day01/main.py
@@ -1,15 +1,50 @@
 import fileinput
+import re
 
-example1 = """"""
+example1 = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+""".split('\n')
 
 def solve1(puzzle):
-    pass
+    s = 0
+    reg = re.compile(r'\d')
+    for line in puzzle:
+        if not line:
+            continue
+        matches = reg.findall(line)
+        if matches:
+            s += int( matches[0] + matches[-1] )
+    print(s)
 
 
-example2 = """"""
+
+
+example2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+eighthree
+""".split('\n')
 
 def solve2(puzzle):
-    pass
+    s = 0
+    reg = re.compile(r'\d|one|two|three|four|five|six|seven|eight|nine')
+    t = {'one': '1', 'two':'2', 'three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+    for line in puzzle:
+        if not line:
+            continue
+        matches = reg.findall(line)
+        if matches:
+            a = matches[0] if matches[0].isnumeric() else t[matches[0]]
+            b = matches[-1] if matches[-1].isnumeric() else t[matches[-1]]
+            print(a,b)
+            s += int(a+b)
+    print(s)
 
 def main():
 
@@ -18,9 +53,9 @@ def main():
     #     line
 
     solve1(example1)
-    # solve1(puzzle)
+    solve1(puzzle)
 
-    # solve2(example2)
+    solve2(example2)
     # solve2(puzzle)
 
 if __name__ == '__main__':
