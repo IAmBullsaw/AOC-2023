a0e3e6403b68208f85fd4c65cff32e4425bd8521
Oskar Jansson <bullsaw1@gmail.com>
2023-12-01 17:32:34 +0100

    Create main.py

---
+++ template/main.py
@@ -0,0 +1,28 @@
+import fileinput
+ 
+example1 = """"""
+ 
+def solve1(puzzle):
+    pass
+ 
+ 
+example2 = """"""
+ 
+def solve2(puzzle):
+    pass
+ 
+def main():
+ 
+    puzzle = list(fileinput.input())
+ 
+    # for line in fileinput.input():
+    #     line
+ 
+    solve1(example1)
+    # solve1(puzzle)
+ 
+    # solve2(example2)
+    # solve2(puzzle)
+ 
+if __name__ == '__main__':
+    main()
