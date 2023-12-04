5bebdaf43f6b227392b8a841f12fefc134a3d8d8
IAmBullsaw <bullsaw1@gmail.com>
2023-12-04 15:16:04 +0100

    [template] updated to use utils

--- template/main.py
+++ template/main.py
@@ -1,30 +1,32 @@
-import fileinput
+import sys
+sys.path.append('../utils/')
+import utils
 
-example1 = """""".split('\n')
+
+example1 = utils.example_as_list("""""")
+answer1 =
 
 def solve1(puzzle):
     pass
 
 
 example2 = """""".split('\n')
+answer2 =
 
 def solve2(puzzle):
     pass
 
 def main():
 
-    puzzle = list(fileinput.input())
-    if not puzzle[-1].strip():
-        puzzle = puzzle[:-1]
+    puzzle = utils.input_as_list(separator=None) # input_as_ints(separator=None)
 
-    # for line in fileinput.input():
-    #     line
+    if not answer1 == solve1(example1):
+        exit(1)
+    solve1(puzzle)
 
-    solve1(example1)
-    # solve1(puzzle)
-
-    # solve2(example2)
-    # solve2(puzzle)
+    if not answer2 == solve2(example2):
+        exit(1)
+    solve2(puzzle)
 
 if __name__ == '__main__':
     main()
